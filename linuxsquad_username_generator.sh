49dedc83a2c6468e0e6cf1e3fa7d2e4386c28b8d
Linuxsquad <ibrahimprogamesyt@gmail.com>
2026-07-15 04:25:56 +0400

    Update linuxsquad_username_generator.sh

--- linuxsquad_username_generator.sh
+++ linuxsquad_username_generator.sh
@@ -1,10 +1,3 @@
-#!/data/data/com.termux/files/usr/bin/bash
-#============================================================#
-#           LINUXSQUAD USERNAME GENERATOR v6.2               #
-#           Termux X11 Panel - Unique Usernames              #
-#           Fixed Display & Output                           #
-#============================================================#
-
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[0;33m'
 BLUE='\033[0;34m'; MAGENTA='\033[0;35m'; CYAN='\033[0;36m'
 WHITE='\033[0;37m'; BOLD='\033[1m'; NC='\033[0m'
